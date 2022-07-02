d7a661d4f2c8de0dec72e3ca6555d0e1ac23abf6
vipinable <vipinable@gmail.com>
2022-07-02 17:56:38 +0200

    fixed terraform code

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "registry.terraform.io/hashicorp/aws"
+      version = "~> 4.19.0"
+    }
+  }
+}
 resource "aws_vpc" "packer" {
   cidr_block       = "172.17.17.0/24"
   instance_tenancy = "default"
