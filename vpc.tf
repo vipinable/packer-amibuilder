a2852e896945f1418dc0bb2ce33e5da4b933b451
vipinable <vipinable@gmail.com>
2022-07-02 18:06:14 +0200

    included block for subnet

--- vpc.tf
+++ vpc.tf
@@ -6,10 +6,25 @@ terraform {
     }
   }
 }
-resource "aws_vpc" "packer" {
+
+data "aws_caller_identity" "current" {}
+data "aws_availability_zones" "available" { state = "available" }
+data "aws_region" "current" {}
+
+resource "aws_default_vpc" "default" {
   cidr_block       = "172.17.17.0/24"
   instance_tenancy = "default"
+  force_destroy = true
   tags = {
-    Name = "packer"
+    Name = "VPC for packer"
+  }
+}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = data.aws_availability_zones.available.names[0]
+  force_destroy = true
+
+  tags = {
+    Name = "Subnet for packer"
   }
 }
