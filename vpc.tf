303568c534093b917c76aa07a9660e5f4b113379
vipinable <vipinable@gmail.com>
2022-07-02 21:27:27 +0200

    fix build issue

--- vpc.tf
+++ vpc.tf
@@ -21,7 +21,7 @@ resource "aws_default_vpc" "default" {
 resource "aws_default_subnet" "default_az1" {
   availability_zone = data.aws_availability_zones.available.names[0]
   force_destroy = true
-
+  depends_on = [ aws_default_vpc.default ]
   tags = {
     Name = "Subnet for packer"
   }
